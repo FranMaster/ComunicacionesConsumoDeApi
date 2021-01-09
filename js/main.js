f5ae423533455e265b08f0cd6f5ed90cef4360eb
Francisco Jimenez Cohen <Francisco.Jimenez.Cohen@gmail.com>
2021-01-09 17:28:45 -0300

    explicacion basica de arrays y consumo de Api de paises Rest Countries

--- js/main.js
+++ js/main.js
@@ -1,14 +1,19 @@
-const arrayPaises=[];
+let arrayPaises=[];
 let TxtPais;
 let TxtUrl;
 let BtnEnviar;
+let BtnBorrar;
 let tblPaises;  
 window.onload=()=>{
      TxtPais=document.querySelector('#Pais');
      TxtUrl=document.querySelector('#UrlImagen');
      BtnEnviar=document.querySelector('#Enviar');
+     BtnBorarr=document.querySelector('#Borrar');
      tblPaises=document.querySelector('#tabla')  
-     BtnEnviar.addEventListener('click',EnviarDatos)    
+     BtnEnviar.addEventListener('click',EnviarDatos)  
+     BtnBorarr.addEventListener('click',BorrarUltimo)  
+     console.log('Cargando....');  
+     CargarDatosApiPaises();     
 }
 let EnviarDatos=()=>{
     arrayPaises.push(
@@ -26,4 +31,29 @@ let llenarTabla=()=>{
         <td>${arrayPaises[arrayPaises.length-1].nombre}</td>
         <td><img src='${arrayPaises[arrayPaises.length-1].pais}' /></td>
     </tr>`;
+}
+
+let llenarTodaLaTabla=()=>{
+    for (let posicion = 0; posicion < arrayPaises.length-1; posicion++) {
+        tblPaises.innerHTML+=`
+        <tr>
+            <td>${arrayPaises[posicion].name}</td>
+            <td><img src='${arrayPaises[posicion].flag}' /></td>
+        </tr>`;        
+    }
+    console.log('Cargado.');  
+}
+
+let CargarDatosApiPaises=async()=>{
+    //realiza una peticion get asincrona
+   let respuesta= await fetch('https://restcountries.eu/rest/v2/all')
+        .then(response => response.json())
+        .then(data =>  data);
+   arrayPaises=respuesta;
+   llenarTodaLaTabla();
+}
+let BorrarUltimo=()=>{
+    arrayPaises.pop()
+    tblPaises.innerHTML="";
+    llenarTodaLaTabla()
 }
